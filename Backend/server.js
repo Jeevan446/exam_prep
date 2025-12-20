5fa2bd18fd3f4f4121c5a6e2efc3001bf9c21d18
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-12-20 21:53:10 +0545

    middleware made for devmode

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const app=express()
 const db=require('./src/db/config.db')
+const demomodeRouter=require('./src/routers/demomode.router')
 require('dotenv').config();
 db();
 
+
+app.use('/api/demomode/',demomodeRouter);
 app.get('/home',(req,res)=>{
 return res.status(200).json({mesage:"Hello world"});
 })
