30d75fd1a4097d0bd3846d0828be19bff037b481
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-12-20 14:35:49 +0545

    server message changed

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const app=express()
 
 
 app.get('/home',(req,res)=>{
-return res.status(200).json({mesage:"Hello world"});
+return res.status(200).json({mesage:"Hello worldsssssssss"});
 })
 app.listen(3000,()=>{
     console.log("App is listening at port 3000")
