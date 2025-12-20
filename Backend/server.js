222df736ac0341c8265b3837d7ed8cede4eea44e
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-12-20 13:26:00 +0545

    initial setup

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express()
+
+
+app.get('/home',(req,res)=>{
+return res.status(200).json({mesage:"Hello world"});
+})
+app.listen(3000,()=>{
+    console.log("App is listening at port 3000")
+})
