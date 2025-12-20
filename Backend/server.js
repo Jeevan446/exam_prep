00d66f436c933af183714a1682e6bb161435eccb
Santosh Majakoti <santoshjr08@gmail.com>
2025-12-20 22:10:51 +0545

    adding comment

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const express=require('express');
 const app=express()
 const db=require('./src/db/config.db')
+const demomodeRouter=require('./src/routers/demomode.router')
 require('dotenv').config();
-db();  //connect db
+db();  //connectection db
 
+
+app.use('/api/demomode/',demomodeRouter);
 app.get('/home',(req,res)=>{
 return res.status(200).json({mesage:"Hello world"});
 })
+
 app.listen(3000,()=>{
     console.log("App is listening at port 3000");
 
