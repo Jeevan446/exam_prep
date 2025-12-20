089d803c3e7e51caa6569c1eb0a3604a19a5bc68
Roman A <adhikariroman01@gmail.com>
2025-12-20 23:16:59 +0545

    Homepage design (initial)

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Footer() {
+    return(
+        <div className=" bg-slate-400  mt-20" >
+            <p className="mt-10 mb-10"> Copyright @ 2025 All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
-
+import NavBar from "../components/NavBar";
+import HomeContent from "../components/HomeContent";
+import Footer from "../components/Footer";
 
 function Homepage() {
     return(
-        <h1>HELLO FROM HOMEPAGE</h1>
+        <div className="flex flex-col justify-baseline">
+            <NavBar />
+            <HomeContent />
+            <Footer />
+        </div>
     );
 }
 export default Homepage;
