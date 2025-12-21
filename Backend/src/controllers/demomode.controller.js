1ce3726c327388e9ad09c044dfa970bdd117d9eb
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-12-21 21:05:48 +0545

    add subject function written

--- Backend/src/controllers/demomode.controller.js
+++ Backend/src/controllers/demomode.controller.js
@@ -1,18 +1,32 @@
-const examtypeModel=require('../models/examtypes.model')
-async function addexamtype(req,res){
-    const {name,subjects}=req.body
-try{
-
-    const examtypefound=await examtypeModel.findOne({name:name})
-    if(examtypefound){
-        return res.status(409).json({message:"Sorry examtype already exists"})
+const examtypeModel = require("../models/examtypes.model");
+const subjectModel = require("../models/subject.model");
+async function addexamtype(req, res) {
+  const { name, subjects } = req.body;
+  try {
+    const examtypefound = await examtypeModel.findOne({ name: name });
+    if (examtypefound) {
+      return res.status(409).json({ message: "Sorry examtype already exists" });
     }
-    await examtypeModel.create({name:name,subjects:subjects})
-return res.status(200).json({message:"New examtype creaated sucessfully"})
-
-}catch(err){
-    console.log("Error at add examtype function ",err)
+    await examtypeModel.create({ name: name, subjects: subjects });
+    return res
+      .status(200)
+      .json({ message: "New examtype creaated sucessfully" });
+  } catch (err) {
+    console.log("Error at add examtype function ", err);
+  }
 }
-}
 
-module.exports={addexamtype}
+async function addSubject(req, res) {
+  const { name, chapters } = req.body;
+  try {
+    const subjectfound = await subjectModel.findOne({ name: name });
+    if (subjectfound) {
+      return res.status(409).json({ message: "Sorry subject already exists" });
+    }
+    await subjectModel.create({ name: name, chapters: chapters });
+    return res.status(200).json({ message: "New subject created sucessfully" });
+  } catch (err) {
+    console.log("Error from addsubject function", err);
+  }
+}
+module.exports = { addexamtype, addSubject };
--- Backend/src/routers/demomode.router.js
+++ Backend/src/routers/demomode.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const{addexamtype} = require('../controllers/demomode.controller');
+const{addexamtype,addSubject} = require('../controllers/demomode.controller');
 
 
 router.post('/addexamtype', addexamtype);
+router.post('/addsubject',addSubject)
 
 module.exports = router;
 
