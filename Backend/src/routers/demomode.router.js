9610b0efe198db467fe83775228d1efc77ed76cc
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-12-27 21:38:07 +0545

    get question route added

--- Backend/src/routers/demomode.router.js
+++ Backend/src/routers/demomode.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const{addexamtype,addSubject,getExamType,getSubjects,addQuestions,addChapter,getChapters} = require('../controllers/demomode.controller');
+const{addexamtype,addSubject,getExamType,getSubjects,addQuestions,addChapter,getChapters,getQuestions} = require('../controllers/demomode.controller');
 
 
 router.post('/addexamtype', addexamtype);
@@ -9,6 +9,7 @@ router.post('/addchapter',addChapter)
 router.get('/getexams',getExamType)
 router.get('/:examtype/subjects',getSubjects)
 router.get('/:examtype/:subjects/chapters',getChapters)
+router.get('/:examtype/:subject/:chapter/questions',getQuestions)
 router.post("/addquestions",addQuestions)
 module.exports = router;
 
