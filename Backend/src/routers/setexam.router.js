497097c878ffe2a03b8cdb6fcefe267c3f3779d7
Santosh Majakoti <santoshjr08@gmail.com>
2026-01-10 20:50:59 +0545

    fix(route):set exam router updated

--- Backend/src/routers/setexam.router.js
+++ Backend/src/routers/setexam.router.js
@@ -2,11 +2,21 @@
 const express = require("express");
 const multer = require("multer");
 const auth =require("../middleware/auth.middleware");
-const { uploadExamSet } = require("../controllers/setexam.controller");
+const { uploadExamSet ,getQuestionsBySet ,getSetsByExamType ,getUniqueExamTypes } = require("../controllers/setexam.controller");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post("/upload",auth, upload.single("file"), uploadExamSet);
+
+router.get("/set-questions/:setId", getQuestionsBySet);
+
+router.get("/examtypes", getUniqueExamTypes);
+
+router.get("/sets/:examType", getSetsByExamType);
+
+
+
+
 
 module.exports = router;
