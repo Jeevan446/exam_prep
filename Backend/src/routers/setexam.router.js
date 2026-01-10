8e60b7dbe39882b4051958a9078777161290156e
Santosh Majakoti <santoshjr08@gmail.com>
2026-01-10 20:12:14 +0545

    fix(sexexam.router): updated on setexam router

--- Backend/src/routers/setexam.router.js
+++ Backend/src/routers/setexam.router.js
@@ -2,11 +2,25 @@
 const express = require("express");
 const multer = require("multer");
 const auth =require("../middleware/auth.middleware");
-const { uploadExamSet } = require("../controllers/setexam.controller");
+const { uploadExamSet ,getSetsByExamType ,getUniqueExamTypes ,getSetNamesByExamType ,getQuestionsBySet} = require("../controllers/setexam.controller");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post("/upload",auth, upload.single("file"), uploadExamSet);
+
+//get all examset according to examptye
+router.get("/examset/:examType", getSetsByExamType);
+
+router.get("/exam-types", getUniqueExamTypes);
+// http://localhost:3000/api/setexam/exam-types 
+
+router.get("/setnames/:examType", getSetNamesByExamType);
+// GET http://localhost:3000/api/setexam/setnames/ioe
+
+router.get("/set-questions/:setId", getQuestionsBySet);
+
+
+
 
 module.exports = router;
