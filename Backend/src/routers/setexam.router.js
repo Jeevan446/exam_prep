d42a1ba96033f966196245fb9fb266d3f99d066d
santosh majakoti <santoshjr08@gmail.com>
2026-01-17 13:23:50 +0545

    feat(upload): enhance exam set upload functionality with validation and new fields

--- Backend/src/routers/setexam.router.js
+++ Backend/src/routers/setexam.router.js
@@ -1,22 +1,16 @@
-
-import  express from "express"
-import  multer from "multer"
-import auth from "../middleware/auth.middleware.js"
-import  { uploadExamSet ,getQuestionsBySet ,getSetsByExamType ,getUniqueExamTypes } from '../controllers/setexam.controller.js'
+import express from "express";
+import multer from "multer";
+import auth from "../middleware/auth.middleware.js";
+import { uploadExamSet, getQuestionsBySet, getSetsByExamType, getUniqueExamTypes } 
+  from '../controllers/setexam.controller.js';
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
-router.post("/upload",auth, upload.single("file"), uploadExamSet);
-
+// Routes
+router.post("/upload", auth, upload.single("file"), uploadExamSet);
 router.get("/set-questions/:setId", getQuestionsBySet);
-
 router.get("/getexamtype", getUniqueExamTypes);
-
 router.get("/sets/:examType", getSetsByExamType);
 
-
-
-
-
-export default router
+export default router;
