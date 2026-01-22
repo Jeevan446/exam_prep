65be0689e91b1999fd6bd293b1d470e06f20884f
santosh majakoti <santoshjr08@gmail.com>
2026-01-22 20:43:43 +0545

    feat(auth):paid user middleware added

---
+++ Backend/src/middleware/checkpaiduser.middleware.js
@@ -0,0 +1,34 @@
+export const checkPaidUser = async (req, res, next) => {
+  try {
+    const { setType } = req.body;
+
+    // Only apply rule for PAID sets
+    if (setType !== "paid") {
+      return next();
+    }
+
+    // req.user comes from auth middleware
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    //  User has not paid
+    if (!req.user.hasPaid) {
+      return res.status(403).json({
+        success: false,
+        message: "Payment required to upload paid exam sets",
+      });
+    }
+
+    //  Paid user
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Payment verification failed",
+    });
+  }
+};
